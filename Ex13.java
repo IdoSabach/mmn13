a41e26fc8daa8276c8614955065f3135514232f6
Ido <Idosabach11@gmail.com>
2024-01-04 14:48:31 +0200

    fix q1

--- Ex13.java
+++ Ex13.java
@@ -1,12 +1,17 @@
+import javax.print.DocFlavor.STRING;
+
 class Ex13 {
 
   public static void main(String[] args) {
     int [] arr = {-8,1,-4,11,9,-15,10,8,-3};
     maxMul3(arr);
 
-    int [] a1 = {1,12,15,26,38};
-    int [] a2 = {12,13,18,30,45};
-    findMedian(a1, a2);
+    // int [] a1 = {1,12,15,26,38};
+    // int [] a2 = {12,13,18,30,45};
+    // findMedian(a1, a2);
+
+    String str = "hello world";
+    System.out.println(str.substring(6));
   }
 
 
@@ -15,6 +20,7 @@ class Ex13 {
   //place = O(1)
   public static int maxMul3 (int [] arr){
     int i=0 , j=1 , x=2 ;
+    int num1=0 , num2=0 , num3=0;
     int sunMul = arr[i] * arr[j] * arr[x];
 
     if(arr.length < 3){
@@ -24,6 +30,9 @@ class Ex13 {
     while(i<arr.length-3){
       if(arr[i] * arr[j] * arr[x] > sunMul){
         sunMul = arr[i] * arr[j] * arr[x];
+        num1 = arr[i];
+        num2 = arr[j];
+        num3 = arr[x];
       }else{
         x++;
       }
@@ -39,14 +48,22 @@ class Ex13 {
         x = j+1;
       }
     }
-    System.out.println(sunMul);
+    System.out.println(num1 + " * " + num2 + " * " + num3 + " = " + sunMul);
     return sunMul;
   }
 
 
-  public static int findMedian (int[] arr1, int[] arr2){
-    int low = 0 , high = arr2.length , mid = (arr1.length + arr2.length)/2;
-    System.out.println(mid);
-    return mid;
-  }
+  // Q2
+  // public static int findMedian (int[] arr1, int[] arr2){
+  //   int low = 0 , high = arr1.length+arr2.length , mid = (arr1.length + arr2.length)/2;
+  //   System.out.println(low);
+  //   System.out.println(mid);
+  //   System.out.println(high);
+
+  //   while(low<=high){
+
+  //   }
+    
+  //   return mid;
+  // }
 }
